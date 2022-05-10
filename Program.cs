673b639d6e1474092a62296cbae894f7b79b18f2
vladislavbezusov <SnyHZZZ@yandex.ru>
2022-05-10 21:17:22 +0300

    first-commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿string[] arr1 = new string[5] {"Vlad", "Hi", "1234654673", "124", "Summer"};
+string[] arr2 = new string[arr1.Length];
+void ArrayWithIf(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if(arr1[i].Length <= 3)
+        {
+            arr2[count] = arr1[i];
+            count++;
+        }
+    }
+}
+void PrintArr(string[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+ArrayWithIf(arr1, arr2);
+PrintArr(arr2);
+
